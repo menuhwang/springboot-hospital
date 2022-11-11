57031f6bfca01016fe61b7091fe9bc9ad9ba7cae
menuHwang <hmin0206@naver.com>
2022-11-11 14:00:31 +0900

    페이지네이션 수정

--- src/main/java/com/likelion/hospital/controller/web/HospitalController.java
+++ src/main/java/com/likelion/hospital/controller/web/HospitalController.java
@@ -1,6 +1,9 @@
 package com.likelion.hospital.controller.web;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hospitals")
 public class HospitalController {
     @GetMapping("")
-    public String index() {
+    public String index(Model model, @PageableDefault Pageable pageable) {
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("size", pageable.getPageSize());
         return "hospitals/index";
     }
 }
