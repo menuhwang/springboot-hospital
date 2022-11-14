e7d24fa317a49c9228d018fbfcf23e4a607d8067
menuHwang <hmin0206@naver.com>
2022-11-14 13:35:22 +0900

    특정 구의 보건 진료소, 보건지소, 보건소 모두 찾기

--- src/main/java/com/likelion/hospital/repository/HospitalRepository.java
+++ src/main/java/com/likelion/hospital/repository/HospitalRepository.java
@@ -11,6 +11,10 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     SELECT * FROM nation_wide_hospitals
          WHERE (full_address LIKE '경기도 수원시%' OR road_name_address LIKE '경기도 수원시%')
            AND nation_wide_hospitals.hospital_name LIKE '%피부%';
+
+    특정 구의 보건 진료소, 보건지소, 보건소 모두 찾기
+    SELECT * FROM nation_wide_hospitals WHERE (full_address LIKE '인천광역시%' OR road_name_address LIKE '인천광역시%')
+           AND business_type_name LIKE '%보건%';
     */
     Page<Hospital> findByHospitalNameContains(String name, Pageable pageable);
     Page<Hospital> findByFullAddressContainsOrFullAddressContains(String city1, String city2, Pageable pageable);
