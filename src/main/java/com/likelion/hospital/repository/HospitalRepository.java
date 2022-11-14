839b08b37ff2ad56ac20a1ed2d8c02fb5217b7da
menuHwang <hmin0206@naver.com>
2022-11-14 13:25:48 +0900

    경기도 수원시 피부과 쿼리 작성

--- src/main/java/com/likelion/hospital/repository/HospitalRepository.java
+++ src/main/java/com/likelion/hospital/repository/HospitalRepository.java
@@ -6,6 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    /*
+    경기도 수원시 피부과
+    SELECT * FROM nation_wide_hospitals
+         WHERE (full_address LIKE '경기도 수원시%' OR road_name_address LIKE '경기도 수원시%')
+           AND nation_wide_hospitals.hospital_name LIKE '%피부%';
+    */
     Page<Hospital> findByHospitalNameContains(String name, Pageable pageable);
     Page<Hospital> findByFullAddressContainsOrFullAddressContains(String city1, String city2, Pageable pageable);
 }
--- src/main/java/com/likelion/hospital/repository/ProductRepository.java
+++ src/main/java/com/likelion/hospital/repository/ProductRepository.java
@@ -4,10 +4,4 @@ import com.likelion.hospital.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    /*
-    경기도 수원시 피부과
-    SELECT * FROM nation_wide_hospitals
-         WHERE (full_address LIKE '경기도 수원시%' OR road_name_address LIKE '경기도 수원시%')
-           AND nation_wide_hospitals.hospital_name LIKE '%피부%';
-    */
 }
