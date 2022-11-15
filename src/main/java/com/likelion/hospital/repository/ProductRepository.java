c85b8b3f6e46397f538724c344d8e1f31a79d4e7
menuHwang <hmin0206@naver.com>
2022-11-15 14:55:59 +0900

    ProductRepository 삭제

--- src/main/java/com/likelion/hospital/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.likelion.hospital.repository;
-
-import com.likelion.hospital.domain.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
