a12b4003a36c5f679a1b197dfa161fa0a8a4a8b9
Ubuntu <ubuntu@ip-172-31-24-158.ap-northeast-2.compute.internal>
2022-11-15 06:02:34 +0000

    배포 스크립트 작성

---
+++ release.sh
@@ -0,0 +1,39 @@
+#!/bin/bas
+NAME=likelion-hospital
+PORT=8080
+
+DATASOURCE_URL=$1
+DATASOURCE_USERNAME=$2
+DATASOURCE_PASSWORD=$3
+
+if [ $# != 3 ];
+then
+	echo "파라미터를 확인해주세요."
+	echo "DATASOURCE_URL:$1"
+	echo "DATASOURCE_USERNAME:$2"
+	echo "DATASOURCE_PASSWORD:$3"
+	exit
+fi
+
+CONTAINER=$(docker ps -aq --filter "name=$NAME")
+
+if [ -n "$CONTAINER" ];
+then
+	echo "컨테이너 종료"
+
+	docker stop $CONTAINER
+
+	echo "컨테이너 삭제"
+
+	docker rm $CONTAINER
+fi
+
+IMAGE=$(docker images --filter=reference="$NAME" -q)
+
+if [ -n "$IMAGE" ]; then
+	docker rmi $IMAGE
+fi
+
+git pull
+docker build -t $NAME . &&
+docker run -d --name $NAME -p $PORT:8080 -e SPRING_DATASOURCE_URL=$DATASOURCE_URL -e SPRING_DATASOURCE_PASSWORD=$DATASOURCE_PASSWORD -e SPRING_DATASOURCE_USERNAME=$DATASOURCE_USERNAME $NAME
