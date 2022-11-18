52cb50e795329d45e48dce3b609f5af68e22673e
menuHwang <hmin0206@naver.com>
2022-11-18 13:43:18 +0900

    HospitalApiController 로깅 추가

--- src/main/java/com/likelion/hospital/controller/api/HospitalApiController.java
+++ src/main/java/com/likelion/hospital/controller/api/HospitalApiController.java
@@ -3,6 +3,7 @@ package com.likelion.hospital.controller.api;
 import com.likelion.hospital.domain.dto.hospital.HospitalResDTO;
 import com.likelion.hospital.service.HospitalService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/hospitals")
@@ -18,11 +20,13 @@ public class HospitalApiController {
 
     @GetMapping("/{id}")
     public ResponseEntity<HospitalResDTO> findById(@PathVariable("id") Integer id) {
+        log.info("병원 조회 id:{}", id);
         return ResponseEntity.ok(hospitalService.findById(id));
     }
 
     @GetMapping("")
     public ResponseEntity<Page<HospitalResDTO>> findAll(@Nullable @RequestParam("city") String city, @PageableDefault Pageable pageable) {
+        log.info("병원 검색 city:{}, page:{}", city, pageable);
         Page<HospitalResDTO> page = (city == null) ? hospitalService.findAll(pageable) : hospitalService.findByCity(city, pageable);
         return ResponseEntity.ok(page);
     }
