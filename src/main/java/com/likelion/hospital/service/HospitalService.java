43c6798e2224af40a8198e598365737bed67c2f2
menuHwang <hmin0206@naver.com>
2022-11-23 17:12:40 +0900

    [Refactor] HospitalService findById 수정
    - HospitalResWithReviewDTO 추가로 인한 수정

--- src/main/java/com/likelion/hospital/service/HospitalService.java
+++ src/main/java/com/likelion/hospital/service/HospitalService.java
@@ -1,11 +1,13 @@
 package com.likelion.hospital.service;
 
 import com.likelion.hospital.domain.dto.hospital.HospitalResDTO;
+import com.likelion.hospital.domain.dto.hospital.HospitalResWithReviewDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface HospitalService {
-    HospitalResDTO findById(Integer id);
+    HospitalResWithReviewDTO findById(Integer id);
+
     Page<HospitalResDTO> findAll(Pageable pageable);
     Page<HospitalResDTO> findByCity(String city, Pageable pageable);
     Page<HospitalResDTO> searchByName(String name, Pageable pageable);
--- src/main/java/com/likelion/hospital/service/HospitalServiceImpl.java
+++ src/main/java/com/likelion/hospital/service/HospitalServiceImpl.java
@@ -1,7 +1,8 @@
 package com.likelion.hospital.service;
 
-import com.likelion.hospital.domain.entity.Hospital;
 import com.likelion.hospital.domain.dto.hospital.HospitalResDTO;
+import com.likelion.hospital.domain.dto.hospital.HospitalResWithReviewDTO;
+import com.likelion.hospital.domain.entity.Hospital;
 import com.likelion.hospital.repository.HospitalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,9 +16,9 @@ public class HospitalServiceImpl implements HospitalService {
     private final HospitalRepository hospitalRepository;
 
     @Override
-    public HospitalResDTO findById(Integer id) {
+    public HospitalResWithReviewDTO findById(Integer id) {
         Hospital hospital = hospitalRepository.findById(id).orElseThrow(() -> new RuntimeException("해당 병원을 찾을 수 없습니다."));
-        return new HospitalResDTO(hospital);
+        return HospitalResWithReviewDTO.of(hospital);
     }
 
     @Override
