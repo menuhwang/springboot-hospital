484c79e6545297919c1ad4c89d871819c0287a02
menuHwang <hmin0206@naver.com>
2022-11-28 13:52:34 +0900

    [Fix] 유저 아이디 unique true

--- src/main/java/com/likelion/hospital/domain/entity/User.java
+++ src/main/java/com/likelion/hospital/domain/entity/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,6 +16,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String userName;
     private String password;
     private String emailAddress;
