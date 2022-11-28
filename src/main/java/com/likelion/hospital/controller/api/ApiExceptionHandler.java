df4219df87cb3caa112e3490ac68efbdf8a10b83
menuHwang <hmin0206@naver.com>
2022-11-28 13:50:36 +0900

    [Feat] 예외 핸들링 수정

--- src/main/java/com/likelion/hospital/controller/api/ApiExceptionHandler.java
+++ src/main/java/com/likelion/hospital/controller/api/ApiExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.likelion.hospital.controller.api;
 
 import com.likelion.hospital.domain.ErrorResponse;
-import com.likelion.hospital.exception.AbstractNotFoundException;
+import com.likelion.hospital.exception.AbstractHttpResponseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
-    @ExceptionHandler(value = AbstractNotFoundException.class)
-    public ResponseEntity<ErrorResponse> dataNotFoundExceptionHandler(AbstractNotFoundException exception) {
+    @ExceptionHandler(value = AbstractHttpResponseException.class)
+    public ResponseEntity<ErrorResponse> dataNotFoundExceptionHandler(AbstractHttpResponseException exception) {
         return ResponseEntity.status(exception.getStatus().value()).body(exception.toErrorResponse());
     }
 }
