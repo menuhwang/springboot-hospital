eaf8648fe3a6bd5d7938bebcd740e4382941593b
menuHwang <hmin0206@naver.com>
2022-11-28 15:54:17 +0900

    [Style] 예외 메시지 수정

--- src/main/java/com/likelion/hospital/exception/forbidden/SignInForbiddenException.java
+++ src/main/java/com/likelion/hospital/exception/forbidden/SignInForbiddenException.java
@@ -2,6 +2,6 @@ package com.likelion.hospital.exception.forbidden;
 
 public class SignInForbiddenException extends AbstractForbiddenException {
     public SignInForbiddenException() {
-        super("아이디 또는 비밀번호를 확인해주세요");
+        super("아이디 또는 비밀번호를 확인해주세요.");
     }
 }
