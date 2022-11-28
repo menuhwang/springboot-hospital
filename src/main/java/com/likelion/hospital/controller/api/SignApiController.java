6223e9135a2bd505c873060d6685d3e531e05952
menuHwang <hmin0206@naver.com>
2022-11-28 15:48:10 +0900

    [Refactor] SignApiController 반환 타입 수정

--- src/main/java/com/likelion/hospital/controller/api/SignApiController.java
+++ src/main/java/com/likelion/hospital/controller/api/SignApiController.java
@@ -1,11 +1,11 @@
 package com.likelion.hospital.controller.api;
 
-import com.likelion.hospital.domain.Response;
 import com.likelion.hospital.domain.dto.user.SignInDTO;
 import com.likelion.hospital.domain.dto.user.SignUpDTO;
 import com.likelion.hospital.domain.dto.user.UserResponse;
 import com.likelion.hospital.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +18,12 @@ public class SignApiController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public Response<UserResponse> join(@RequestBody SignUpDTO dto) {
-        return Response.success(userService.join(dto));
+    public ResponseEntity<UserResponse> join(@RequestBody SignUpDTO dto) {
+        return ResponseEntity.ok(userService.join(dto));
     }
 
     @PostMapping("/signin")
-    public Response<UserResponse> login(@RequestBody SignInDTO dto) {
-        return Response.success(userService.login(dto));
+    public ResponseEntity<UserResponse> login(@RequestBody SignInDTO dto) {
+        return ResponseEntity.ok(userService.login(dto));
     }
 }
