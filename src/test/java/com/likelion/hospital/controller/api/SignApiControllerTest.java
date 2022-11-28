247455773a6099a84b70ef5ccbff10b33447329c
menuHwang <hmin0206@naver.com>
2022-11-28 15:53:37 +0900

    [Test] SignApiController 로그인 테스트

--- src/test/java/com/likelion/hospital/controller/api/SignApiControllerTest.java
+++ src/test/java/com/likelion/hospital/controller/api/SignApiControllerTest.java
@@ -1,9 +1,11 @@
 package com.likelion.hospital.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.hospital.domain.dto.user.SignInDTO;
 import com.likelion.hospital.domain.dto.user.SignUpDTO;
 import com.likelion.hospital.domain.dto.user.UserResponse;
 import com.likelion.hospital.exception.conflict.DuplicateUsernameException;
+import com.likelion.hospital.exception.forbidden.SignInForbiddenException;
 import com.likelion.hospital.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,9 +77,56 @@ class SignApiControllerTest {
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.message").value("이미 존재하는 아이디입니다."))
                 .andDo(print());
+        verify(userService).join(any(SignUpDTO.class));
     }
 
     @Test
-    void login() {
+    void login_정상() throws Exception {
+        String USERNAME = "test";
+        String EMAIL = "test@gmail.com";
+        String PASSWORD = "password";
+        SignInDTO signInDTO = SignInDTO.builder()
+                .userName(USERNAME)
+                .password(PASSWORD)
+                .build();
+
+        UserResponse userResponse = UserResponse.builder()
+                .userName(USERNAME)
+                .emailAddress(EMAIL)
+                .build();
+
+        given(userService.login(any(SignInDTO.class))).willReturn(userResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signInDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userName").value(USERNAME))
+                .andExpect(jsonPath("$.emailAddress").value(EMAIL))
+                .andDo(print());
+
+        verify(userService).login(any(SignInDTO.class));
+    }
+
+    @Test
+    void login_비밀번호_불일치() throws Exception {
+        String USERNAME = "test";
+        String EMAIL = "test@gmail.com";
+        String PASSWORD = "password";
+        SignInDTO signInDTO = SignInDTO.builder()
+                .userName(USERNAME)
+                .password(PASSWORD)
+                .build();
+
+        given(userService.login(any(SignInDTO.class))).willThrow(new SignInForbiddenException());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signInDTO)))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value("아이디 또는 비밀번호를 확인해주세요."))
+                .andDo(print());
+
+        verify(userService).login(any(SignInDTO.class));
     }
 }
