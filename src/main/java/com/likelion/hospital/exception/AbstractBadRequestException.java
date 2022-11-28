a8fe35a2767f8ec27f3620713040800d444a1f8f
menuHwang <hmin0206@naver.com>
2022-11-28 14:21:41 +0900

    [Style] abstract 누락 수정

--- src/main/java/com/likelion/hospital/exception/AbstractBadRequestException.java
+++ src/main/java/com/likelion/hospital/exception/AbstractBadRequestException.java
@@ -2,7 +2,7 @@ package com.likelion.hospital.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class AbstractBadRequestException extends AbstractHttpResponseException {
+public abstract class AbstractBadRequestException extends AbstractHttpResponseException {
     public AbstractBadRequestException(String message) {
         super(HttpStatus.BAD_REQUEST, message);
     }
