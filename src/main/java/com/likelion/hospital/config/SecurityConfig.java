f6fc6ec866abd9575c66fcd2421c40b67a467acd
menuHwang <hmin0206@naver.com>
2022-11-29 14:49:20 +0900

    [Style] 불필요한 어노테이션 삭제

--- src/main/java/com/likelion/hospital/config/SecurityConfig.java
+++ src/main/java/com/likelion/hospital/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.likelion.hospital.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
