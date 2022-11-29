9394501ebd7afd1e51347f18cba3f420e9ac27c8
menuHwang <hmin0206@naver.com>
2022-11-29 14:15:19 +0900

    [Feat] 스프링 시큐리티 적용

---
+++ src/main/java/com/likelion/hospital/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.likelion.hospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.cors();
+
+        http.authorizeHttpRequests()
+                .antMatchers("/api/v1/hospitals/**").permitAll()
+                .antMatchers("/api/v1/users/signup", "/api/v1/users/signin").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
