c96c1ab8bab53aa232aa382fc2bb6810b24a1ce9
menuHwang <hmin0206@naver.com>
2022-12-08 11:48:15 +0900

    [Refactor] UserController 분리

---
+++ src/main/java/com/likelion/hospital/controller/web/UserController.java
@@ -0,0 +1,24 @@
+package com.likelion.hospital.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "users/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "users/logout";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "users/mypage";
+    }
+}
--- src/main/java/com/likelion/hospital/controller/web/WebController.java
+++ src/main/java/com/likelion/hospital/controller/web/WebController.java
@@ -9,14 +9,4 @@ public class WebController {
     public String index() {
         return "index";
     }
-
-    @GetMapping("/users/login")
-    public String login() {
-        return "users/login";
-    }
-
-    @GetMapping("/users/logout")
-    public String logout() {
-        return "users/logout";
-    }
 }
